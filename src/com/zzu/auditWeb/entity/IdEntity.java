1003b9facc5f4fd8df15f780abdaaf365835e2fd
Captain-He-pc <ishezhichao@gmail.com>
2020-09-24 15:58:19 +0800

    原生登录

---
+++ src/com/zzu/auditWeb/entity/IdEntity.java
@@ -0,0 +1,21 @@
+package com.zzu.auditWeb.entity;
+
+public abstract class IdEntity {
+    protected long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "IdEntity [id=" + id + ", getId()=" + getId() + ", getClass()="
+                + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+                + super.toString() + "]";
+    }
+
+}
